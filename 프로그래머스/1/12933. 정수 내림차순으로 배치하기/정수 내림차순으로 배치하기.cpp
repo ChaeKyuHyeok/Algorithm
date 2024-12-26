f1dc16ab2f214cbbc25753538417e2ec793dd8dc
Malnal <rbgur211@naver.com>
2024-12-26 14:37:35 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    string temp = to_string(n);
+    
+    sort(temp.rbegin(), temp.rend());
+    answer = stoll(temp);
+    
+    return answer;
+}
