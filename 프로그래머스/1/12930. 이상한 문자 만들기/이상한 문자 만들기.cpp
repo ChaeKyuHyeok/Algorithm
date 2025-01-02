e4035b4c03d4134b9d07418f0ab37fce10ad5b7d
Malnal <rbgur211@naver.com>
2025-01-02 10:56:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int count = 0;
+    
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i] == ' ')
+        {
+            count = 0;
+            continue;
+        }
+        if(count % 2 == 0)
+        {
+            s[i] = toupper(s[i]);
+            count++;
+        }
+        else if(count % 2 == 1)
+        {
+            s[i] = tolower(s[i]);
+            count++;
+        }
+    }
+    answer = s;
+    return answer;
+}
