9aff0dc9faff62d66dcdfc29890c8647cd115a92
Malnal <rbgur211@naver.com>
2025-01-13 09:21:51 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    int remain = 0;
+    
+    while(n>=a)
+    {
+        answer += (n/a)*b;
+        remain = n%a;
+        n = (n/a)*b + remain;
+    }
+    return answer;
+}
